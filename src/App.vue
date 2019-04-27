177eaec9d7a00e50d52e04cc888ef558272831a6
mosaic141688 <mbhamalimosaic@gmail.com>
2019-04-27 14:03:38 +0200

    Getting ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat @click="create_dialog=true" target="_blank">
+        <span class="mr-2" >New Spot</span>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+      <newspot :dialog="create_dialog" @close="create_dialog=false" @save="save">
+        
+      </newspot>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
+import newspot from "./components/newspot";
+import firebase from 'firebase'
+
+ var storage = firebase.storage();
 
 export default {
-  name: 'app',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    newspot
+  },
+  data() {
+    return {
+      create_dialog: false
+    };
+  },
+  methods: {
+    save() {}
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
